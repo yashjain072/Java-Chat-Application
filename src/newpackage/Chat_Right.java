0b763c11fab67efce229c72cc5cb564c6c4e0d20
Yash Jain <60340147+yashjain072@users.noreply.github.com>
2022-09-15 09:51:08 +0530

    Add files via upload

---
+++ src/newpackage/Chat_Right.java
@@ -0,0 +1,76 @@
+package newpackage;
+
+import java.awt.Color;
+import javax.swing.Icon;
+
+public class Chat_Right extends javax.swing.JLayeredPane {
+
+    public Chat_Right() {
+        initComponents();
+        txt.setBackground(new Color(179, 233, 255));
+    }
+
+    public void setText(String text) {
+        if (text.equals("")) {
+            txt.hideText();
+        } else {
+            txt.setText(text);
+        }
+        txt.seen();
+    }
+/*
+    public void setImage(Icon... image) {
+        txt.setImage(true, image);
+    }
+
+    public void setImage(String... image) {
+        txt.setImage(false, image);
+    }
+
+    public void setFile(String fileName, String fileSize) {
+        txt.setFile(fileName, fileSize);
+    }
+*/
+    public void setTime() {
+        txt.setTime("10:30 PM");    //  Testing
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        txt = new newpackage.Chat_Item();
+
+        setLayer(txt, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(txt, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(txt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+    }
+    /*
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 32, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 16, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+*/
+    private newpackage.Chat_Item txt;
+}
